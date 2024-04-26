39904b6c07191fca363c646b34e72446ea8d9b7e
Kieran Taylor <kieran.taylor@dxc.com>
2024-04-26 11:44:44 +0100

    Font edits

--- js/script.js
+++ js/script.js
@@ -1,4 +1,31 @@
+var listOfDescriptions = ["Fabulous","Slay","Girl Boss", "Just a Girl", "Queen", "Bew-ree-full", "Wicked af", "The Best"]
+
+
 $('#toggle').click(function() {
    $(this).toggleClass('active');
    $('#overlay').toggleClass('open');
   });
+
+
+ async function updateStuff() {
+
+
+   for (const element of listOfDescriptions) {
+      await updateDescription(element);
+   }
+   
+   updateStuff();
+  }
+
+
+async function updateDescription(elementName) {
+   document.getElementById("descriptionid").innerHTML = elementName;
+   await sleep(1500);
+}
+
+  function sleep(ms) {
+   return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+
+
