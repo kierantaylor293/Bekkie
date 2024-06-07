13c87206c44063c66c83f5d91844caac72fb903c
Kieran Taylor <kieran.taylor@dxc.com>
2024-06-07 21:26:34 +0100

    Checking after birthday

--- js/script.js
+++ js/script.js
@@ -30,7 +30,7 @@ function updateCount() {
 
 
 
-   var countDownDate = new Date(2024,5,7,0,0,0).getTime();
+   var countDownDate = new Date(2024,5,6,0,0,0).getTime();
 
    // Update the count down every 1 second
    var x = setInterval(function() {
